2338db75ae07c69dc8a3e08c5c77573fc3492f7c
Bo Zhang <bo@bytelegend.com>
2021-03-24 21:40:44 +0800

    Increase timeout for opensource server
    It's usually the first test to run so pulling images takes ~1 min.
    Increase the timeout to 120s.

--- server-opensource/src/test/kotlin/com/bytelegend/app/opensourceserver/AppBrowserSmokeTest.kt
+++ server-opensource/src/test/kotlin/com/bytelegend/app/opensourceserver/AppBrowserSmokeTest.kt
@@ -29,7 +29,7 @@ class AppBrowserSmokeTest : AbstractBrowserTest() {
         browserWebDriverContainers.add(createBrowser({ ChromeOptions() }, buildTmpDir))
     }
 
-    @Timeout(60)
+    @Timeout(120)
     @Test
     fun `anonymous access and login`() {
         browserWebDriverContainers[0].start()
