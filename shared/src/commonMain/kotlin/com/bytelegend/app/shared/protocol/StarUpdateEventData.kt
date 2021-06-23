7d97615045606396e562b0f486fe8748f1081ce2
Bo Zhang <bo@bytelegend.com>
2021-06-23 22:51:38 +0800

    Refactor GameWebSocketServer

--- shared/src/commonMain/kotlin/com/bytelegend/app/shared/protocol/StarUpdateEventData.kt
+++ shared/src/commonMain/kotlin/com/bytelegend/app/shared/protocol/StarUpdateEventData.kt
@@ -4,6 +4,10 @@ import com.bytelegend.app.shared.entities.PlayerMission
 import com.bytelegend.app.shared.entities.PlayerMissionAnswer
 import com.bytelegend.app.shared.entities.mission.OnFinishSpec
 
+interface ServerAware {
+    val server: Int
+}
+
 interface UnicastEventData {
     val playerId: String
 }
@@ -13,21 +17,24 @@ data class StarUpdateEventData(
     val map: String,
     val missionId: String,
     val change: Int,
-    val newValue: Int
-) : UnicastEventData
+    val newValue: Int,
+    override val server: Int = 0
+) : UnicastEventData, ServerAware
 
 data class MissionUpdateEventData(
     override val playerId: String,
     val map: String,
     val change: PlayerMissionAnswer,
-    val newValue: PlayerMission
-) : UnicastEventData
+    val newValue: PlayerMission,
+    override val server: Int = 0
+) : UnicastEventData, ServerAware
 
 data class ItemsStatesUpdateEventData(
     override val playerId: String,
     val missionId: String,
-    val onFinishSpec: OnFinishSpec
-) : UnicastEventData
+    val onFinishSpec: OnFinishSpec,
+    override val server: Int = 0
+) : UnicastEventData, ServerAware
 
 data class KickOffEventData(
     override val playerId: String,
