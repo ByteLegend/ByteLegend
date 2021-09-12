1412badb50c352c911862fe37ed5dc8f710b47de
Bo Zhang <bo@bytelegend.com>
2021-09-12 13:35:30 +0800

    Remove lastUpdatedIn and lastUpdatedMs
    We no longer need them.

--- server-shared/common/src/main/kotlin/com/bytelegend/app/servershared/dal/StoredInPlayerTable.kt
+++ server-shared/common/src/main/kotlin/com/bytelegend/app/servershared/dal/StoredInPlayerTable.kt
@@ -1,12 +1,12 @@
 /*
  * Copyright 2021 ByteLegend Technologies and the original author or authors.
- * 
+ *
  * Licensed under the GNU Affero General Public License v3.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      https://github.com/ByteLegend/ByteLegend/blob/master/LICENSE
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,21 +19,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore
 import software.amazon.awssdk.enhanced.dynamodb.Key
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbIgnore
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue
-
-val REPLICABLE_LAST_UPDATED_IN_KEY = "lastUpdatedIn"
-val REPLICABLE_LAST_UPDATED_MS_KEY = "lastUpdatedMs"
-
-interface Replicable {
-    /**
-     * The region name where the record is updated recently, e.g. cn-north-1
-     */
-    val lastUpdatedIn: String
-
-    /**
-     * Last updated timestamp, in epoch milliseconds.
-     */
-    val lastUpdatedMs: Long
-}
 
 // This is an adapter layer for application and database layer
 // For Player, the sk is "P"
