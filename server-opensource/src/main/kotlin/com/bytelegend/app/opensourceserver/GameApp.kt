8774cc38a6f10f9dd05d5a1b1d767014f7f263f5
Bo Zhang <bo@bytelegend.com>
2022-03-13 11:39:14 +0800

    Remove unused ServerLocation

--- server-opensource/src/main/kotlin/com/bytelegend/app/opensourceserver/GameApp.kt
+++ server-opensource/src/main/kotlin/com/bytelegend/app/opensourceserver/GameApp.kt
@@ -90,7 +90,6 @@ class IndexController(
                     PlayerContext.get(),
                     Locale.EN,
                     "/static",
-                    ServerLocation.Seoul,
                     ""
                 )
             )
