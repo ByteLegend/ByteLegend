c92a664adb5d13c4145ba3035638e89f406f5a3d
Bo Zhang <bo@bytelegend.com>
2022-03-28 17:42:29 +0800

    Create pull request based on the ref when player opens the web editor window

--- shared/src/commonMain/kotlin/com/bytelegend/app/shared/entities/ChallengeCodeAnswer.kt
+++ shared/src/commonMain/kotlin/com/bytelegend/app/shared/entities/ChallengeCodeAnswer.kt
@@ -19,6 +19,17 @@ package com.bytelegend.app.shared.entities
 data class ChallengeCodeAnswer(
     // e.g. https://github.com/a/b/pull/12345
     val pullRequestHtmlUrl: String?,
+
+    // this is only used when we need to create a new pull request to avoid potential conflicts
+    // for example:
+    // 1. Repository is version A.
+    // 2. Player 1 opens version A and starts working on it.
+    // 3. Player 2 opens version A, changes it to version B, submits it, then repository becomes version B.
+    // 4. Player 1 submits code (version C) based on version A, creates a pull request.
+    //    Now the pull request is comparing version B <-> version C. It should be comparing version A <-> version C because version A is the "fork point" for player 1.
+    //    in this case, this baseRef is used for player 1 to create a pull request.
+    val baseRef: String?,
+
     // key: file path, value: changed file
     // e.g.
     // {"src/main/com/bytelegend/Main.java", "package com.bytelegend..."}
